0121b5183e15a85a00398b6d52e6bc1132b091a9
td2thinh <td2thinh@gmail.com>
2024-11-03 22:49:11 +0100

    removed Nat contrusctor (not needed right now), working on unification algo

--- lib/termTypes.ml
+++ lib/termTypes.ml
@@ -2,3 +2,6 @@ type lambda_term =
   | Var of string
   | Abs of string * lambda_term
   | App of lambda_term * lambda_term
+
+type lambda_type = TVar of string | TArrow of lambda_type * lambda_type
+(* | TNat *)
