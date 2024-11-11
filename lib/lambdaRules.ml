883e355a4657e676a9d1ba4086b92b61d92d5cd8
td2thinh <td2thinh@gmail.com>
2024-11-11 23:53:19 +0100

    working on eval PCF

--- lib/lambdaRules.ml
+++ lib/lambdaRules.ml
@@ -25,15 +25,15 @@ let alpha_conversion (term : lambda_term) : lambda_term =
         let new_var = fresh_var () in
         Abs (new_var, aux t (StringMap.add x new_var var_map))
     | App (t1, t2) -> App (aux t1 var_map, aux t2 var_map)
-    (* | Let (x, t1, t2) ->
-           let new_var = fresh_var () in
-           Let (new_var, aux t1 var_map, aux t2 (StringMap.add x new_var var_map))
-       | IfZero (t1, t2, t3) ->
-           IfZero (aux t1 var_map, aux t2 var_map, aux t3 var_map)
-       | IfEmpty (t1, t2, t3) ->
-           IfEmpty (aux t1 var_map, aux t2 var_map, aux t3 var_map)
-       | List l -> List (L.map (fun x -> aux x var_map) l)
-       | _ -> term *)
+    | Let (x, t1, t2) ->
+        let new_var = fresh_var () in
+        Let (new_var, aux t1 var_map, aux t2 (StringMap.add x new_var var_map))
+    | IfZero (t1, t2, t3) ->
+        IfZero (aux t1 var_map, aux t2 var_map, aux t3 var_map)
+    | IfEmpty (t1, t2, t3) ->
+        IfEmpty (aux t1 var_map, aux t2 var_map, aux t3 var_map)
+    | List l -> List (L.map (fun x -> aux x var_map) l)
+    | _ -> term
   in
   aux term var_map
 
@@ -45,24 +45,21 @@ let rec substitution (var : string) (new_term : lambda_term)
   | Var x -> if x = var then new_term else term
   | App (t1, t2) ->
       App (substitution var new_term t1, substitution var new_term t2)
-  | Abs (x, t) ->
-      let new_var = fresh_var () in
-      Abs (new_var, substitution var new_term (substitution x (Var new_var) t))
-(* | Let (x, t1, t2) ->
-       if x = var then term
-       else Let (x, substitution var new_term t1, substitution var new_term t2)
-   | IfZero (t1, t2, t3) ->
-       IfZero
-         ( substitution var new_term t1,
-           substitution var new_term t2,
-           substitution var new_term t3 )
-   | IfEmpty (t1, t2, t3) ->
-       IfEmpty
-         ( substitution var new_term t1,
-           substitution var new_term t2,
-           substitution var new_term t3 )
-   | List l -> List (L.map (fun x -> substitution var new_term x) l)
-   | _ -> term *)
+  | Abs (x, t) -> Abs (x, substitution var new_term t)
+  | Let (x, t1, t2) ->
+      Let (x, substitution var new_term t1, substitution var new_term t2)
+  | IfZero (t1, t2, t3) ->
+      IfZero
+        ( substitution var new_term t1,
+          substitution var new_term t2,
+          substitution var new_term t3 )
+  | IfEmpty (t1, t2, t3) ->
+      IfEmpty
+        ( substitution var new_term t1,
+          substitution var new_term t2,
+          substitution var new_term t3 )
+  | List l -> List (L.map (fun x -> substitution var new_term x) l)
+  | _ -> term
 
 (* Beta reduction using the Left to Right - Call by Value strategy *)
 (* Reduce to lambda expressions, only reduce Applications
@@ -84,6 +81,16 @@ let rec ltr_cbv_step (term : lambda_term) : lambda_term option =
           | None -> None))
   | Abs (x, t) -> (
       match ltr_cbv_step t with Some t' -> Some (Abs (x, t')) | None -> None)
+  | List l -> (
+      match l with
+      | [] -> None
+      | x :: xs -> (
+          match ltr_cbv_step x with
+          | Some x' -> Some (List (x' :: xs))
+          | None -> (
+              match ltr_cbv_step (List xs) with
+              | Some xs' -> Some (List (x :: xs'))
+              | None -> None)))
   | _ -> None
 
 let max_steps = 300
--- lib/lambdaUtils.ml
+++ lib/lambdaUtils.ml
@@ -5,35 +5,34 @@ let print_term (term : lambda_term) : string =
     match term with
     | Var x -> x
     | Abs (x, t) -> Printf.sprintf "(λ%s.%s)" x (aux t)
-    | App (t1, t2) -> Printf.sprintf "(%s %s)" (aux t1) (aux t2)
-    (* | App (t1, t2) -> (
-           match t1 with
-           | App (t1_1, t1_2) -> (
-               match t1_1 with
-               | Add (t1_1_1, t1_1_2) ->
-                   Printf.sprintf "(%s + %s) %s" (aux t1_1_1) (aux t1_1_2)
-                     (aux t1_2)
-               | Sub (t1_1_1, t1_1_2) ->
-                   Printf.sprintf "(%s - %s) %s" (aux t1_1_1) (aux t1_1_2)
-                     (aux t1_2)
-               | Cons (t1_1_1, t1_1_2) ->
-                   Printf.sprintf "(%s :: %s) %s" (aux t1_1_1) (aux t1_1_2)
-                     (aux t1_2)
-               | _ -> Printf.sprintf "(%s %s)" (aux t1) (aux t2))
-           | _ -> Printf.sprintf "(%s %s)" (aux t1) (aux t2))
-       | IfZero (t1, t2, t3) ->
-           Printf.sprintf "(if0 %s then %s else %s)" (aux t1) (aux t2) (aux t3)
-       | IfEmpty (t1, t2, t3) ->
-           Printf.sprintf "(ifE %s then %s else %s)" (aux t1) (aux t2) (aux t3)
-       | List l -> Printf.sprintf "[%s]" (String.concat "; " (List.map aux l))
-       | Cons (t1, t2) -> Printf.sprintf "(%s :: %s)" (aux t1) (aux t2)
-       | Head t -> Printf.sprintf "head %s" (aux t)
-       | Tail t -> Printf.sprintf "tail %s" (aux t)
-       | Let (x, t1, t2) -> Printf.sprintf "let %s = %s in %s" x (aux t1) (aux t2)
-       | Val n -> string_of_int n
-       | Fix t -> Printf.sprintf "fix %s" (aux t)
-       | Add (t1, t2) -> Printf.sprintf "(%s + %s)" (aux t1) (aux t2)
-       | Sub (t1, t2) -> Printf.sprintf "(%s - %s)" (aux t1) (aux t2) *)
+    | App (t1, t2) -> (
+        match t1 with
+        | App (t1_1, t1_2) -> (
+            match t1_1 with
+            | Add (t1_1_1, t1_1_2) ->
+                Printf.sprintf "(%s + %s) %s" (aux t1_1_1) (aux t1_1_2)
+                  (aux t1_2)
+            | Sub (t1_1_1, t1_1_2) ->
+                Printf.sprintf "(%s - %s) %s" (aux t1_1_1) (aux t1_1_2)
+                  (aux t1_2)
+            | Cons (t1_1_1, t1_1_2) ->
+                Printf.sprintf "(%s :: %s) %s" (aux t1_1_1) (aux t1_1_2)
+                  (aux t1_2)
+            | _ -> Printf.sprintf "(%s %s)" (aux t1) (aux t2))
+        | _ -> Printf.sprintf "(%s %s)" (aux t1) (aux t2))
+    | IfZero (t1, t2, t3) ->
+        Printf.sprintf "(if0 %s then %s else %s)" (aux t1) (aux t2) (aux t3)
+    | IfEmpty (t1, t2, t3) ->
+        Printf.sprintf "(ifE %s then %s else %s)" (aux t1) (aux t2) (aux t3)
+    | List l -> Printf.sprintf "[%s]" (String.concat "; " (List.map aux l))
+    | Cons (t1, t2) -> Printf.sprintf "(%s :: %s)" (aux t1) (aux t2)
+    | Head t -> Printf.sprintf "head %s" (aux t)
+    | Tail t -> Printf.sprintf "tail %s" (aux t)
+    | Let (x, t1, t2) -> Printf.sprintf "let %s = %s in %s" x (aux t1) (aux t2)
+    | Val n -> string_of_int n
+    | Fix t -> Printf.sprintf "fix %s" (aux t)
+    | Add (t1, t2) -> Printf.sprintf "(%s + %s)" (aux t1) (aux t2)
+    | Sub (t1, t2) -> Printf.sprintf "(%s - %s)" (aux t1) (aux t2)
   in
   aux term
 
@@ -51,11 +50,26 @@ let alpha_equal t1 t2 =
         alpha_eq new_env t1' t2'
     | App (t1a, t1b), App (t2a, t2b) ->
         alpha_eq env t1a t2a && alpha_eq env t1b t2b
-    (* | Int n1, Int n2 -> n1 = n2
-       | Add (t1a, t1b), Add (t2a, t2b) ->
-           alpha_eq env t1a t2a && alpha_eq env t1b t2b
-       | Sub (t1a, t1b), Sub (t2a, t2b) ->
-           alpha_eq env t1a t2a && alpha_eq env t1b t2b *)
+    (* From here on out it's my code *)
+    | Let (x1, t1', t1''), Let (x2, t2', t2'') ->
+        alpha_eq env t1' t2' && alpha_eq ((x1, x2) :: env) t1'' t2''
+    | Val n1, Val n2 -> n1 = n2
+    | Fix t1', Fix t2' -> alpha_eq env t1' t2'
+    | Add (t1a, t1b), Add (t2a, t2b) ->
+        alpha_eq env t1a t2a && alpha_eq env t1b t2b
+    | Sub (t1a, t1b), Sub (t2a, t2b) ->
+        alpha_eq env t1a t2a && alpha_eq env t1b t2b
+    | IfZero (t1a, t1b, t1c), IfZero (t2a, t2b, t2c) ->
+        alpha_eq env t1a t2a && alpha_eq env t1b t2b && alpha_eq env t1c t2c
+    | IfEmpty (t1a, t1b, t1c), IfEmpty (t2a, t2b, t2c) ->
+        alpha_eq env t1a t2a && alpha_eq env t1b t2b && alpha_eq env t1c t2c
+    | List l1, List l2 ->
+        List.length l1 = List.length l2
+        && List.for_all2 (fun x y -> alpha_eq env x y) l1 l2
+    | Cons (t1a, t1b), Cons (t2a, t2b) ->
+        alpha_eq env t1a t2a && alpha_eq env t1b t2b
+    | Head t1, Head t2 -> alpha_eq env t1 t2
+    | Tail t1, Tail t2 -> alpha_eq env t1 t2
     | _ -> false
   in
   alpha_eq [] t1 t2
